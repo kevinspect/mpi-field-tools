5200a4789f1c2324d1a99462b86649378cc899c3
kevinspect <kev@michiganpropertyinspections.com>
2026-08-24 21:22:39 -0400

    Refresh offline cache for informed-party multi-select

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "mpi-field-tools-shell-v15";
+const CACHE_NAME = "mpi-field-tools-shell-v16";
 const APP_SHELL = ["./", "./index.html", "./site.webmanifest", "./apple-touch-icon.png", "./icon-192.png", "./icon-512.png"];
 
 async function fetchFresh(resource) {
