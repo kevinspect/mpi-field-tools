4bc1e8ebc3ba2c0bf5b32f2a48e33a458276ee20
kevinspect <kev@michiganpropertyinspections.com>
2026-08-24 20:48:09 -0400

    Refresh offline cache for damage email recipient update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "mpi-field-tools-shell-v12";
+const CACHE_NAME = "mpi-field-tools-shell-v13";
 const APP_SHELL = ["./", "./index.html", "./site.webmanifest", "./apple-touch-icon.png", "./icon-192.png", "./icon-512.png"];
 
 async function fetchFresh(resource) {
