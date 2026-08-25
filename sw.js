26edab4350e11f979fb6b470ae0a93886a48f3b7
kevinspect <kev@michiganpropertyinspections.com>
2026-08-24 20:15:30 -0400

    Refresh offline cache for comment builder update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "mpi-field-tools-shell-v8";
+const CACHE_NAME = "mpi-field-tools-shell-v9";
 const APP_SHELL = ["./", "./index.html", "./site.webmanifest", "./apple-touch-icon.png", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", event => {
