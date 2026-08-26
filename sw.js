113f424e3d11403bbacdf0c6f2cab5d36f6ab35e
kevinspect <kev@michiganpropertyinspections.com>
2026-08-26 02:54:07 -0400

    Guide inspectors to the next MPI refresher

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "mpi-field-tools-shell-v54";
+const CACHE_NAME = "mpi-field-tools-shell-v55";
 const APP_SHELL = ["./", "./index.html", "./site.webmanifest", "./apple-touch-icon.png", "./icon-192.png", "./icon-512.png", "./mpi-logo.png", "./tool-thumbnails.png"];
 
 async function fetchFresh(resource) {
