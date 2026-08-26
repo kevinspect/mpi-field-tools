a2a421d19cf4b951f283537eae17466cb58b74f7
kevinspect <kev@michiganpropertyinspections.com>
2026-08-26 05:25:32 -0400

    Build 90: clarify empty training achievements

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "mpi-field-tools-shell-v89";
+const CACHE_NAME = "mpi-field-tools-shell-v90";
 const APP_SHELL = ["./", "./index.html", "./site.webmanifest", "./apple-touch-icon.png", "./icon-192.png", "./icon-512.png", "./mpi-logo.png", "./tool-thumbnails.png"];
 
 async function fetchFresh(resource) {
