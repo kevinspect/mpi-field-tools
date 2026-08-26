a7b81433e7c8c44c9927da9086bb75dbe7639c13
kevinspect <kev@michiganpropertyinspections.com>
2026-08-26 00:08:07 -0400

    Finish Today’s Jobs connection

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "mpi-field-tools-shell-v33";
+const CACHE_NAME = "mpi-field-tools-shell-v34";
 const APP_SHELL = ["./", "./index.html", "./site.webmanifest", "./apple-touch-icon.png", "./icon-192.png", "./icon-512.png", "./mpi-logo.png", "./tool-thumbnails.png"];
 
 async function fetchFresh(resource) {
